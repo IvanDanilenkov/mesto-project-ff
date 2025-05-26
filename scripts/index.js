c2f37d5946216d4d5a98b8b4518e0b1c2aa5b816
Ivan Danilenkov <danilich071@gmail.com>
2025-05-26 02:14:09 -0400

    feat(index.js): add create and remove card functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard({ name, link }) {
+  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const image = cardElement.querySelector(".card__image");
+  const title = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  title.textContent = name;
+  image.alt = name;
+  image.src = link;
+  
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+};
 
 // @todo: Функция удаления карточки
+ function deleteCard (cardElement) {
+  cardElement.remove();
+}; 
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  const card = createCard(element);
+  cardContainer.append(card);
+});
