762ba469de684b25125336591f867014d6f65f51
Ivan Danilenkov <danilich071@gmail.com>
2025-06-28 18:16:00 -0400

    refactor(webpack.config) Шрифты, изображения, стили разделены по директориям

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '',
+    publicPath: '/',
   },
   mode: 'development',
   devServer: {
@@ -25,9 +25,19 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name].[hash][ext]',
       },
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'fonts/[name].[hash][ext]',
+      },
+    },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
@@ -46,7 +56,8 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-
+    new MiniCssExtractPlugin({
+    filename: 'styles/[name].[contenthash].css'
+    }),
   ]
 }
